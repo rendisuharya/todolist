a0d2f7f151ab9c576b4286695d46131d923a7547
Projekukk <rendisuharya05@gmail.com>
2025-04-11 11:22:17 +0700

    ada perbaruan di bagian page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import TodoList from '../components/todolist';
+
 
 function page() {
   return (
     <>
       <h1>Landing Page </h1>
+      <TodoList />
     </>
   );
 }
